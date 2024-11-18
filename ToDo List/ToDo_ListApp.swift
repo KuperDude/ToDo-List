b2e79a3b37f148b97f9ee92a4a260f2675e18b13
KuperDude <fieldsdev06@gmail.com>
2024-11-19 01:43:23 +0300

    Initial Commit

---
+++ ToDo List/ToDo_ListApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo List
+//
+//  Created by MyBook on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
